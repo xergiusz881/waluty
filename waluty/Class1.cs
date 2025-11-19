0ce63a306334e97cd7e6b17676f2278db58d2401
Adam Łyszcz <adam.lyszcz@teb.edu.pl>
2025-11-19 10:28:53 +0100

    commit 2

---
+++ waluty/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace waluty
+{
+    internal class Class1
+    {
+        public string currency { get; set; }
+        public string code { get; set; }
+        public double mid { get; set; }
+    }
+}
---
+++ waluty/apirespons.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace waluty
+{
+    internal class apirespons
+    {
+        public string table { get; set; }
+        public string no { get; set; }
+        public string effectiveDate { get; set; }
+        public List<Class1> rates { get; set; }
+    }
+}
