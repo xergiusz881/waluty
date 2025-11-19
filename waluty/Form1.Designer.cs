8a94ea4ecbd0f3531256d488cd791c609645b701
Adam Łyszcz <adam.lyszcz@teb.edu.pl>
2025-11-19 10:15:02 +0100

    komit 1

--- waluty/Form1.Designer.cs
+++ waluty/Form1.Designer.cs
@@ -28,12 +28,42 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            richTextBox1 = new RichTextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(41, 398);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "pobierz api";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(12, 12);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(776, 380);
+            richTextBox1.TabIndex = 2;
+            richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(richTextBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+        private Button button1;
+        private RichTextBox richTextBox1;
     }
 }
--- waluty/Form1.cs
+++ waluty/Form1.cs
@@ -6,5 +6,21 @@ namespace waluty
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://api.nbp.pl/");
+
+                string query = "api/exchangerates/tables/A";
+
+                HttpResponseMessage response = client.GetAsync(query).Result;
+
+                string json = response.Content.ReadAsStringAsync().Result;
+
+                richTextBox1.Text = json;
+            }
+        }
     }
 }
